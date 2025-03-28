d574728f805010cc2852fbf99d2c4b29ac62b8d0
Jamiiiiii <jami34@windowslive.com>
2025-03-28 14:30:09 +0200

    Updated for more up to date info about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,11 +11,10 @@ export const About = () => {
     </div>
     <ul className={styles.aboutItems}>
         <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Curosr icon" />
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
                 <h3>Frontend Developer</h3>
-                <p>I'm Frontend developer with experience in building responsive and optimized sites
-
+                <p>As a Frontend Developer, I specialize in building dynamic and visually appealing user interfaces using React, TypeScript, JavaScript, HTML, and CSS. I strive to create high-performance, responsive websites with reusable components that enhance the user experience, ensuring seamless interactions across all devices.
                 </p>
             </div>
         </li>
@@ -23,7 +22,7 @@ export const About = () => {
             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
             <div className={styles.aboutItemText}> 
                 <h3>Backend Developer</h3>
-                <p>I'm Backend developer with experience in building responsive and optimized sites
+                <p>With experience in backend development, I focus on building robust and scalable server-side applications using Node.js and various modern technologies. I prioritize performance, security, and maintainability to deliver solutions that support seamless integrations and optimized workflows.
                     
                 </p>
             </div>
@@ -32,7 +31,7 @@ export const About = () => {
             <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
                 <h3>UI Designer</h3>
-                <p>I'm UI Desingner with experience in designing responsive and beautiful sites
+                <p>As a UI Designer, I craft intuitive, user-centered designs that bring digital experiences to life. My expertise lies in creating visually stunning, responsive layouts that adapt seamlessly to different screen sizes. I aim to design beautiful, functional interfaces that ensure a smooth user journey across all platforms.
                     
                 </p>
             </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,9 @@ export const Hero = () => {
   <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Jami</h1>
-        <p className={styles.description}>I'm fourth-year Information Technology student at Oulu University of Applied Science. I'm looking for a internship/work in IT. Preferred roles would be Front-End or Full-Stack Developer. Check my projects below and contact me if you are interested!
+        <p className={styles.description}>I'm fourth-year Information Technology student at Oulu University of Applied Science. I'm looking for junior/entry level work in IT. Preferred roles would be Front-End or Full-Stack Developer. Check my projects below and contact me if you are interested!
         </p>
-        <a href="mailto:jami34@windowslive.com" className={styles.contactBtn}>Contact me
+        <a href="mailto:jami34@windowslive.com" className={styles.contactBtn}>Contact me by e-mail
         </a>
         </div>
         <img src={getImageUrl("hero/heroImage.png")} alt="Hero image of me" className={styles.heroImg} 
