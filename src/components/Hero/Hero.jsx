9193d008772ca4164b85e77b060964f7a3eef0aa
Jamiiiiii <jami34@windowslive.com>
2025-05-14 16:10:04 +0300

    Updated bio

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   <section className={styles.container}>
     <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Jami</h1>
-        <p className={styles.description}>I'm fourth-year Information Technology student at Oulu University of Applied Science. I'm looking for junior/entry level work in IT. Preferred roles would be Front-End or Full-Stack Developer. Check my projects below and contact me if you are interested!
+        <p className={styles.description}>I have a Bachelor's degree in Business and Information Systems. I'm looking for a junior or entry-level position in IT. My preferred roles are Front-End or Full-Stack Developer. Feel free to check out my projects and certificates below, and contact me if you're interested!
         </p>
         <a href="mailto:jami34@windowslive.com" className={styles.contactBtn}>Contact me by e-mail
         </a>
