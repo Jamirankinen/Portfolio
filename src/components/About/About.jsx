04c644861afed606def7adf493f659acd4f3b29c
Jamiiiiii <jami34@windowslive.com>
2025-05-15 18:43:55 +0300

    Animations trigger faster on mobile

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -61,7 +61,7 @@ export const About = () => {
               className={styles.description}
               initial="hidden"
               whileInView="visible"
-              viewport={{ once: true, amount: 0.8 }}
+              viewport={{ once: true, amount: 0.5 }}
               variants={{
                 hidden: {},
                 visible: {
@@ -120,7 +120,7 @@ export const About = () => {
               className={styles.description}
               initial="hidden"
               whileInView="visible"
-              viewport={{ once: true, amount: 0.8 }}
+              viewport={{ once: true, amount: 0.5 }}
               variants={{
                 hidden: {},
                 visible: {
@@ -179,7 +179,7 @@ export const About = () => {
               className={styles.description}
               initial="hidden"
               whileInView="visible"
-              viewport={{ once: true, amount: 0.8 }}
+              viewport={{ once: true, amount: 0.5 }}
               variants={{
                 hidden: {},
                 visible: {
