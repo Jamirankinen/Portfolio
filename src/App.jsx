ca96f3faf4a063d5dc14a55c7807c52c500d6940
Jamiiiiii <jami34@windowslive.com>
2025-05-19 13:52:50 +0300

    Loadings per section

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,25 @@ function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      <Suspense fallback={<div className={styles.loading}>Loading...</div>}>
+      <Suspense fallback={<div className={styles.loading}>Loading pixels...</div>}>
         <Hero />
+        </Suspense>
+        <Suspense fallback={<div className={styles.loading}>Building in process</div>}>
         <About />
+        </Suspense>
+        <Suspense fallback={<div className={styles.loading}>Loading animations</div>}>
         <Experience />
+        </Suspense>
+        <Suspense fallback={<div className={styles.loading}>Loading all the stuff</div>}>
         <Projects />
+        </Suspense>
+        <Suspense fallback={<div className={styles.loading}>Loading stuff again</div>}>
         <Certificates />
+        </Suspense>
+        <Suspense fallback={<div className={styles.loading}>Loading more stuff</div>}>
         <Contact />
-      </Suspense>
+        </Suspense>
+      
       <ScrollToTopButton />
     </div>
   );
